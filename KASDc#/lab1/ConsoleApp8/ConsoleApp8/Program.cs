edf8f83e01bba51ec71026996d30e1fe34fec6b3
six6hroun <gura0516@gmail.com>
2025-12-30 21:08:14 +0300

    Add files via upload

---
+++ KASDc#/lab1/ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,50 @@
+﻿string text = "Z://xd.txt";
+StreamReader sr = new StreamReader(text);
+
+int n;
+n = Convert.ToInt32(sr.ReadLine()); // считываем размероность
+
+double[,] matr = new double[n, n]; // считываем матрицу
+for (int i = 0; i < n; i++)
+{
+    string[] line1 = sr.ReadLine().Split(" ");
+    for (int j = 0; j < n; j++)
+    {
+        matr[i, j] = double.Parse(line1[j]);
+    }
+}
+
+double[] vector = new double[n]; // считываем вектор
+string[] line2 = sr.ReadLine().Split(" ");
+for (int i = 0; i < n; i++)
+{
+    vector[i] = double.Parse(line2[i]);
+}
+
+bool Simetr(double[,] matr) // проверка на симетричность
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matr[i, j] != matr[j, i]) return false;
+        }
+    }
+    return true;
+}
+
+double length = 0;
+if (Simetr(matr))
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            length += vector[i] * matr[i, j] * vector[j]; //длина вектора
+        }
+    }
+    length = Math.Sqrt(length); // извлекаем корень
+Console.WriteLine($"Vector length: {length}");
+}
+else Console.WriteLine("h");
+
